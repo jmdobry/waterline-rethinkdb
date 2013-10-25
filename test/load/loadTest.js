7a0f7c2514b0e1b7967561690d230c2d3c1bbc16
Jason Dobry <jason.dobry@gmail.com>
2013-10-24 21:14:36 -0600

    LoadTest now succeeds. Swapped single connection for a connection pool. #5

--- test/load/loadTest.js
+++ test/load/loadTest.js
@@ -1,11 +1,11 @@
 'use strict';
-var Adapter = require('../../lib/adapter'),
+var RethinkDBAdapter = require('../../lib/adapter'),
 	Config = require('../support/config'),
 	Fixture = require('../support/fixture'),
 	assert = require('assert'),
 	async = require('async');
 
-var CONNECTIONS = 10000;
+var CONNECTIONS = 300;
 
 describe('Load Testing', function () {
 	// test should finish in under 1 minute
@@ -14,19 +14,22 @@ describe('Load Testing', function () {
 	before(function (done) {
 		var Schema;
 
-		Adapter.drop('test', function (err) {
+		RethinkDBAdapter.drop('test', function (err) {
 			if (err) {
+				console.error(err);
 				return done(err);
 			} else {
 
 				// Register The Collection
-				Adapter.registerCollection({ identity: 'test', config: Config }, function (err) {
+				RethinkDBAdapter.registerCollection({ identity: 'test', config: Config }, function (err) {
 					if (err) {
+						console.error(err);
 						done(err);
 					} else {
 						// Define The Collection
-						Adapter.define('test', Fixture, function (err, schema) {
+						RethinkDBAdapter.define('test', Fixture, function (err, schema) {
 							if (err) {
+								console.error(err);
 								done(err);
 							} else {
 								Schema = schema;
@@ -42,17 +45,14 @@ describe('Load Testing', function () {
 	describe('create with x connection', function () {
 
 		it('should not error', function (done) {
-
 			// generate x users
 			async.times(CONNECTIONS, function (n, next) {
-
 				var data = {
 					first_name: Math.floor((Math.random() * 100000) + 1),
 					last_name: Math.floor((Math.random() * 100000) + 1),
 					email: Math.floor((Math.random() * 100000) + 1)
 				};
-
-				Adapter.create('test', data, next);
+				RethinkDBAdapter.create('test', data, next);
 			}, function (err, users) {
 				assert(!err);
 				assert(users.length === CONNECTIONS);
